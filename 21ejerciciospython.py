997f398561c28833594816f7a8f65d33a15d5d63
hermeskalid1-art <hermeskalid1@gmail.com>
2026-01-17 14:43:03 +0000

    cambios 20-21

---
+++ 21ejerciciospython.py
@@ -0,0 +1,5 @@
+print("Sofía va con su abuela a un campo para recoger manzanas. Al final de la tarde, Sofía recogió 23 manzanas, mientras que su abuela recogió 35 manzanas cuantas manzanas recogieron en total")
+manzanasdesofia1=23
+manzanasdelaabuela2=35
+totaldemanzanas=manzanasdesofia1 + manzanasdelaabuela2
+print("el total de manzanas es")
