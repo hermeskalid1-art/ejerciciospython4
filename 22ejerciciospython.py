1e140e656c51ce149042db5c97c53b60fb287270
hermeskalid1-art <hermeskalid1@gmail.com>
2026-01-17 15:53:32 +0000

    cambios22{23

---
+++ 22ejerciciospython.py
@@ -0,0 +1,7 @@
+print("una empresa de productos lacteos ocupa, para vender y distribuir leche, camiones con una capacidad de carga de 250 cajas, cada una de ellas contiene 12 litros y el precio de litro es de $10, si un supermercado realiza un pedido de 4 cargas. ¿cuanto debe pagar por la compra del lacteo a la empresa?")
+capacidaddecamiones=250
+contienecadauna=12
+preciodelitro=10
+pedidodecargasdelsupermercado=4
+deberadepagarporellacteo=capacidaddecamiones * contienecadauna * preciodelitro * pedidodecargasdelsupermercado
+print("debera pagar el super por el encargo")
---
+++ 23ejerciciospython.py
@@ -0,0 +1,7 @@
+print("En la reparticion de una herencia el abuelo designa en partes iguales un terreno de 12 hectarias a 3 de sus nietos, si el precio por metro cuadrado es de $250,¿cual es el monto que recibio cada uno de los herederos?(Considera una hectarea igual a 10 000 m2.)")
+designoenpartesiguales=12
+designoasusnietos=3
+preciodelmetrocuadraro=250
+unahectareaesiguala=10000
+montoquerecibieroncadaunodelosherederos=designoenpartesiguales * unahectareaesiguala / designoasusnietos * preciodelmetrocuadraro
+print("el monto que recibira cada uno de los herederos sera",montoquerecibieroncadaunodelosherederos)
